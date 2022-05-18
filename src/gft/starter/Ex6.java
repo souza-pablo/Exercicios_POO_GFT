42410d9f70c402c0da9dc02dfe6b36aaa291dc44
souza-pablo <pablo.souza.5@outlook.com.br>
2022-05-18 16:59:11 -0300

    add ex6

---
+++ src/gft/starter/Ex6.java
@@ -0,0 +1,16 @@
+package gft.starter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Map<String, Integer> listaPessoas = new HashMap<>(){{
+            put("Leandro", 21);
+            put("Jessica", 18);
+        }};
+        System.out.println(listaPessoas);
+        System.out.println("Conferindo se o nome Jessica está na lista... " + listaPessoas.containsKey("Jessica") +
+                ". Sua idade é " + listaPessoas.get("Jessica"));
+    }
+}
